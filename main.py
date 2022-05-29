c944784de8a7d7f459d1ec4343a354e5cae2e8a4
awkward-asya <asya7sazonova@gmail.com>
2022-05-29 15:36:43 +0300

    added an icon for the app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 from wind import Main
 from planet_request import planet_list_generator
@@ -7,6 +8,7 @@ from planet_request import planet_list_generator
 if __name__ == '__main__':
     arr = planet_list_generator()
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('icon.png'))
     print('start')
     ex = Main(arr)
     ex.show()
